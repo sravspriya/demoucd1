bf98dd7e2fc4f42abc687e514e489f8910b9ef39
sravspriya <suriti@miraclesoft.com>
2016-04-21 11:13:12 -0400

    gg

---
+++ src/bank/Bank.java
@@ -0,0 +1,35 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bank;
+
+/**
+ *
+ * @author miracle
+ */
+public class Bank {
+
+    /**
+    public class Bank
+{
+String b-name;
+String b_loc;
+public void initBank(String gbn,String bl)
+{
+b_name=bn;
+b_loc=bl;
+}
+public void display()
+{
+System.out.println("The bank name is"+b_name);
+
+System.out.println("The bank name is"+b_loc);
+}
+public static void main(String args[])
+{
+Bank b=new Bank;
+b.initBank("SBI","visakhapatnam");
+b.display();
+
+}
